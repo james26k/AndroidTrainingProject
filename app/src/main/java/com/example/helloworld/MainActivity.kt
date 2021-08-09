260ddbb2200b58a9cb5dd70de4501fe875316422
james26k <cb26.hayashi@gmail.com>
2021-08-10 00:45:59 +0900

    update: ボタンクリックでサイコロの画像をランダム表示

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +16,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt: Int = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1    -> R.drawable.dice_1
+            2    -> R.drawable.dice_2
+            3    -> R.drawable.dice_3
+            4    -> R.drawable.dice_4
+            5    -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }

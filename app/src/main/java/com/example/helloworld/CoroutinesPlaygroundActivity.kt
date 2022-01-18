a5528ce1318983767bb3bd000364220258f2d44f
james26k <cb26.hayashi@gmail.com>
2022-01-19 02:19:55 +0900

    add; コルーチンを利用するためのactivityを追加

---
+++ app/src/main/java/com/example/helloworld/CoroutinesPlaygroundActivity.kt
@@ -0,0 +1,11 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoroutinesPlaygroundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutines_playground)
+    }
+}

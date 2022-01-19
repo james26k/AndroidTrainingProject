71407dfe218b4db1d7efa50954deaaf81ea5ff56
james26k <cb26.hayashi@gmail.com>
2022-01-20 00:11:04 +0900

    add: MainActicityに練習用の各Acticity名を並べるListViewを追加

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    private val activityNames = arrayOf(
+        "DiceRoll",
+        "CoroutinesPlayground"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.project_list)
+        val arrayAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_expandable_list_item_1,
+            activityNames
+        )
+        listView.adapter = arrayAdapter
     }
 }
